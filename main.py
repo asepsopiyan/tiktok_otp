c77ac6670bb723ec28f77983d33e2a321086e823
Asep Sopiyan <asep.sopiyan1309@gmail.com>
2023-10-02 23:57:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-import threading
 
+import threading
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,70 +10,61 @@ proxies = [
     "https://dmproxy698:dmproxy698@209.99.134.241:5937",
     "https://dmproxy698:dmproxy698@45.114.15.244:6225",
 ]
-def automate_browser(phone_number,  proxy):
-    options = webdriver.ChromeOptions()
-    # options.add_experimental_option("detach", True)
-    options.add_argument(f'--proxy-server={proxy}')
 
+
+def automate_browser(phone_number, proxy):
+    options = webdriver.ChromeOptions()
+    options.add_argument(f'--proxy-server={proxy}')
 
     # for debugging force awake
     driver = webdriver.Chrome(options=options)
-    # driver = webdriver.Chrome()
     driver.get("https://www.tiktok.com/404?fromUrl=/phone-or-email")
 
-
     btn_login = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="header-login-button"]'))
-    ).click()
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="header-login-button"]'))
+    )
+    btn_login.click()
 
     btn_phone = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="loginContainer"]/div/div/a[2]'))
-    ).click()
-
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="loginContainer"]/div/div/a[2]'))
+    )
+    btn_phone.click()
 
     btn_code_country = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[2]/div/div[1]/div'))
-    ).click()
-
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[2]/div/div[1]/div'))
+    )
+    btn_code_country.click()
 
     input_code_country = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="login-phone-search"]'))
-    ).send_keys("timor")
-
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="login-phone-search"]'))
+    )
+    input_code_country.send_keys("timor")
 
     btn_code_country = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="TL-670"]'))
-    ).click()
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="TL-670"]'))
+    )
+    btn_code_country.click()
 
     input_number = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[2]/div/div[2]/input'))
-    ).send_keys(phone_number)
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[2]/div/div[2]/input'))
+    )
+    input_number.send_keys(phone_number)
 
     btn_send_code = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[3]/div/button'))
-    ).send_keys(phone_number)
-
-
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[3]/div/button'))
+    )
+    btn_send_code.send_keys(phone_number)
 
 
 # Read numbers from an Excel file with a header named "number"
 df = pd.read_excel('number.xlsx', header=0)  # Assuming the header is in the first row
 numbers = df['number'].tolist()  # Extract the numbers from the 'number' column
 
-
 total_numbers = len(numbers)  # Total number of data points
 max_browsers = len(proxies)
 
 # Split the list of numbers into chunks of max_browsers size
 number_chunks = [numbers[i::max_browsers] for i in range(max_browsers)]
-
 
 for i, chunk in enumerate(number_chunks):
     threads = []
@@ -98,4 +88,4 @@ for i, chunk in enumerate(number_chunks):
 
 print(" ")
 print("----")
-print(f"All browser closed.")
+print(f"All browsers closed.")
